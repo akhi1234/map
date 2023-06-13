4cc90d7354ea9f1128d519036671cbcaa66f33a9
akhilesh <sonkarakhilesh02@gmail.com>
2023-06-13 14:28:34 +0530

    added open street map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 import './App.css';
+import Map from "./Map.js";
+
 
 function App() {
+  const [zoomLevel, setZoomLevel] = useState(0);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-header'>
+      <Button variant="primary" onClick={handleShow}>
+        Open Map
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Map</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Zoom level: {zoomLevel}x
+          <button onClick={() => setZoomLevel(zoomLevel + 1)}>+</button>
+          <button onClick={() => setZoomLevel(zoomLevel - 1)}>-</button>
+          <hr />
+          <Map zoomLevel={zoomLevel} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+         
+        </Modal.Footer>
+      </Modal>
+    </div>
     </div>
   );
 }
